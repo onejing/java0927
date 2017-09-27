c5b51ed79a5a39a81e9c6f023542a8f4282bf6b5
one.jing <gloriaaa0226@yahoo.com.tw>
2017-09-27 20:19:12 +0800

    ｍｏｄｉｆｙ　ｍｙ　ｃｏｄｅ　ａｇａｉｎ

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("106024578王晶");
         System.out.println("王晶");
+        System.out.println("");
     }
 }
 
